3d6294d145e2035d031c785c0f075361a50ca9e6
azakosta <azakost@gmail.com>
2024-05-21 18:25:26 +0500

    fix

--- lib/common/light_theme.dart
+++ lib/common/light_theme.dart
@@ -3,4 +3,19 @@ import 'package:flutter/material.dart';
 final lightTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
   useMaterial3: true,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: WidgetStateProperty.resolveWith((state) {
+        if (state.contains(WidgetState.disabled)) {
+          return const Color(0xffB0B0B0);
+        }
+        return const Color(0xff1D3557);
+      }),
+      foregroundColor: const WidgetStatePropertyAll(Color(0xffFEFEFE)),
+      shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      )),
+      minimumSize: WidgetStateProperty.all(const Size(254, 65.0)),
+    ),
+  ),
 );
--- lib/features/init/page.dart
+++ lib/features/init/page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../root/page.dart';
-
-
 class InitPage extends StatelessWidget {
   const InitPage({super.key});
 
@@ -11,6 +8,7 @@ class InitPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
         padding: const EdgeInsets.all(30.0),
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -20,27 +18,26 @@ class InitPage extends StatelessWidget {
         ),
         child: Column(
           children: [
-            const SizedBox(width: double.infinity),
-            const Spacer(flex: 2,),
-            SvgPicture.asset('assets/images/welcome-logo-ph.svg'),
+            const Spacer(flex: 2),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 315),
+              child: SvgPicture.asset(
+                'assets/images/welcome-logo-ph.svg',
+              ),
+            ),
             const Spacer(),
             SafeArea(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const RootPage(),
-                    ));
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: const WidgetStatePropertyAll(Color(0xff1D3557)),
-                    foregroundColor: const WidgetStatePropertyAll(Color(0xffFEFEFE)),
-                    shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )),
-                    padding: const WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: 25.0, horizontal: 60.0))
-                  ),
-                  
-                  child: const Text('Давайте начнем!')
+              child: ElevatedButton(
+                child: const Text(
+                  'Давайте начнем!',
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/root');
+                },
+              ),
             ),
             const Spacer(),
           ],
